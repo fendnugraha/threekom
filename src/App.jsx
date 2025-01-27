9f2b52b12584210a2b00b8db24bccafc46ddb018
fendnugraha <fendnugraha92@gmail.com>
2025-01-27 16:05:26 +0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="h-screen flex flex-col justify-center items-center w-screen">
+            <h1 className="text-8xl text-blue-600 font-bold">
+                THREE<span className="text-orange-600">KOMUNIKA</span>
+            </h1>
+            <h4 className="font-bold">Layanan Transaksi Keuangan</h4>
+        </div>
+    );
+}
+
+export default App;
